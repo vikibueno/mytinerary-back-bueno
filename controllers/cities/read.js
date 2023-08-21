60c5ac4ba15e46a8a93854873185959bfd0814a1
Vik <mviki.bueno@gmail.com>
2023-08-21 14:59:22 -0300

    populate

--- controllers/cities/read.js
+++ controllers/cities/read.js
@@ -2,7 +2,11 @@ import City from '../../models/City.js'
 
 export default async (req,res) => {
     try {
-        let allCities = await City.find()
+        //let allCities = await City.find().populate('admin_id','photo name mail -_id')
+        //let allCities = await City.find({},'country  city photo smalldescription admin_id').populate('admin_id','photo name mail -_id')
+
+        let allCities = await City.find().select('country  city photo smalldescription admin_id').populate('admin_id','photo name mail -_id')
+
         return res.status(200).json({
             success: true,
             message: 'cities found',
